c083fd5ed3023bb09659456c9fb5d0a59ea27d4c
justinsasek <justinsasek@gmail.com>
2022-11-28 19:12:34 -0600

    a

--- Week_9/homework backend/firebase.js
+++ Week_9/homework backend/firebase.js
@@ -1,10 +1,15 @@
+const { storage } = require("firebase-admin");
+var { getStorage } = require("firebase-admin/storage");
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./cred.json");
 
-admin.initializeApp({
+const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  storageBucket: "gs://todo-list-be033.appspot.com"
 });
 
 const firestore = admin.firestore();
-module.exports = { firestore };
+const stor = getStorage(app);
+console.log(stor);
+module.exports = { firestore, stor};
--- Week_9/homework backend/routes/pic.js
+++ Week_9/homework backend/routes/pic.js
@@ -1,14 +1,22 @@
 
 /** Module for handling pics */
 const firebase = require("../firebase");
+var admin = require('firebase-admin');
+const { getStorage, ref, uploadBytes } = require("firebase/storage");
+
+const fs = require('fs');
 const express = require("express");
 const cors = require("cors");
 const middleware = require('../middleware/functions');
 const { random } = require("underscore");
 const { addConsoleHandler } = require("selenium-webdriver/lib/logging");
 const db = firebase.firestore;
+const storage = firebase.stor;
 const multer = require("multer");
-
+// const { ref } = require("firebase-admin");
+// var { getStorage } = require("firebase-admin/stor");
+// const { storage } = require("firebase-admin");
+// var admin = require("firebase-admin");
 
 // Define route and middleware
 const pic = express.Router();
@@ -28,27 +36,51 @@ pic.use(express.json());
 
 
 
-pic.post("/", multer().single("pic"), middleware.authMiddleware, async (req, res) => {
+pic.post("/", multer().single("pic"), async (req, res) => { //middleware.authMiddleware,
   console.log('adding pic');
   const file = req.file;
   if (file.mimetype != 'image/jpeg' && file.mimetype != 'image/png') {
     res.status(401).send("Not jpeg or png");
     return;
   }
-  console.log(file);
+  // console.log(file.fieldname);
 
-  var storageRef = firebase.storage().ref();
-  var ref = storageRef.child('images/space.jpg');
-  ref.put(file.buffer).then((snapshot) => {
-    console.log('Uploaded an array!');
-  })
-    .then(() => {
-      res.status(201).send("image updated");
-    })
-    .catch((error) => {
-      console.error("Error writing document: ", error);
-      res.status(400).send("Failed to add");
-    });
+  if (file == null)
+    return;
+
+  console.log(storage)
+  const url = 'gs://todo-list-be033.appspot.com/Space.png';
+  const storageRef = ref(storage, url);
+  // const storageRef = ref(stor, 'gs://todo-list-be033');
+  // const storageRef = ref(mainRef, 'Space.png');
+  console.log(storageRef);
+  uploadBytes(storageRef, file)
+  .then((snapshot) => {
+    console.log('Uploaded a blob or file!');
+  });
+  // stor.put(file) //.ref(`/images/${file.fieldname}`)
+  //   .on("state_changed", alert("success"), alert);
+
+  // admin.initializeApp({
+  //   credential: admin.credential.cert(serviceAccount), storageBucket: "gs://databases-demo-bd424.appspot.com",
+  // });
+  // var bucket = getStorage().bucket();
+
+  // storage.ref(`/images/profile`)
+  // .put(file)
+  //   .on("state_changed", alert("success"), alert);
+
+  // var ref = storageRef.child('images/space.jpg');
+  // ref.put(file.buffer).then((snapshot) => {
+  //   console.log('Uploaded an array!');
+  // })
+  //   .then(() => {
+  //     res.status(201).send("image updated");
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error writing document: ", error);
+  //     res.status(400).send("Failed to add");
+  //   });
   // uid = Math.floor(Math.random() * (999999 - 111111 + 1)) + 111111;
 
   // db.collection("pic-items").doc('' + uid).set({
